c48982fb067e24b67e420d3d73bb205c32a010fc
Tim <lmg89is@gmail.com>
2018-04-24 13:58:46 +0700

    "Framework" Clone

--- public/index.php
+++ public/index.php
@@ -33,7 +33,7 @@ $modules = [ROOT, APP, CORE, CONTROLLER, DB, MODEL, VIEW, SETTINGS ];
 set_include_path(get_include_path() . PATH_SEPARATOR . implode(PATH_SEPARATOR, $modules));
 spl_autoload_register('spl_autoload', false);
 #var_dump(get_include_path());
-#var_dump($_SERVER['REQUEST_URI']);
+var_dump($_SERVER['REQUEST_URI']);
 #var_dump(CONTROLLER);
 
 New Application;
